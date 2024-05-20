479edc1ac57a72c5ca6ba6fe833001066721597b
18120021 <166790112+18120021@users.noreply.github.com>
2024-05-20 15:49:30 +0700

    Update final_server_run.py

--- final_server_run.py
+++ final_server_run.py
@@ -3,39 +3,19 @@ import threading
 import webbrowser
 import subprocess
 import time
+import os
 from http.server import SimpleHTTPRequestHandler
 
 # Define the port for the HTTP server
 PORT = 8001
 STREAMLIT_PORT = 8502
 
-# Define the handler to serve the HTML file and handle starting Streamlit
-class Handler(SimpleHTTPRequestHandler):
+# Define the handler to serve the HTML file
+class Handler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
         if self.path == '/':
             self.path = 'index.html'
-            if self.path == '/start-streamlit':
-                self.start_streamlit_app()
-                self.send_response(200)
-                self.end_headers()
-                self.wfile.write(b'Streamlit app is starting...')
-                return
-        return SimpleHTTPRequestHandler.do_GET(self)
-
-    def start_streamlit_app(self):
-        if not is_streamlit_running():
-            threading.Thread(target=start_streamlit, daemon=True).start()
-
-# Function to check if Streamlit is already running
-def is_streamlit_running():
-    import requests
-    try:
-        response = requests.get(f'http://localhost:{STREAMLIT_PORT}')
-        if response.status_code == 200:
-            return True
-    except requests.ConnectionError:
-        return False
-    return False
+        return http.server.SimpleHTTPRequestHandler.do_GET(self)
 
 # Function to start the HTTP server
 def start_http_server():
@@ -45,12 +25,13 @@ def start_http_server():
 
 # Function to start the Streamlit app
 def start_streamlit():
-    print("Starting Streamlit app...")
-    process = subprocess.Popen(['streamlit', 'run', '/Users/Nauroha/Downloads/TA/main2.py', '--server.port', str(STREAMLIT_PORT)])
-    process.communicate()
+    os.system('streamlit run /Users/Nauroha/Downloads/TA/main2.py.py --server.port {STREAMLIT_PORT}')
 
 # Start the HTTP server in a separate thread
-threading.Thread(target=start_http_server, daemon=True).start()
+threading.Thread(target=start_http_server).start()
+
+# Start the Streamlit app
+start_streamlit()
 
 # Give the server a moment to start
 time.sleep(1)
